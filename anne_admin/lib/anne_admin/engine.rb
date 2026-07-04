4fcff748af8a7878b6a1e4ecea2c2f6174c34073
kykt35 <kykt35@gmail.com>
2026-07-04 18:07:48 +0900

    fix: ignore anne admin resource files in zeitwerk

--- anne_admin/lib/anne_admin/engine.rb
+++ anne_admin/lib/anne_admin/engine.rb
@@ -2,6 +2,18 @@ module AnneAdmin
   class Engine < ::Rails::Engine
     isolate_namespace AnneAdmin
 
+    initializer "anne_admin.ignore_resource_paths", before: :set_autoload_paths do |app|
+      next unless defined?(Rails.autoloaders)
+
+      [
+        app.root.join("app/admin/resources"),
+        app.root.join("config/anne_admin/resources")
+      ].each do |path|
+        Rails.autoloaders.main.ignore(path)
+        Rails.autoloaders.once.ignore(path)
+      end
+    end
+
     initializer "anne_admin.load_resources" do |app|
       app.config.to_prepare do
         AnneAdmin.load_resources!
