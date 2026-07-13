ef506e53267dab8da7ca5cc2a2e6da91e37cf024
kykt35 <kykt35@gmail.com>
2026-07-13 10:38:32 +0900

    chore(reservation-management): scaffold the example host app

---
+++ examples/resavation_management/config/application.rb
@@ -0,0 +1,27 @@
+require_relative "boot"
+
+require "rails"
+require "active_model/railtie"
+require "active_record/railtie"
+require "active_job/railtie"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+
+Bundler.require(*Rails.groups)
+
+require "anne_auth"
+require "anne_admin"
+require "anne_access"
+
+module ReservationManagement
+  class Application < Rails::Application
+    config.load_defaults 8.1
+
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :utc
+    config.i18n.default_locale = :ja
+    config.hosts.clear
+    config.secret_key_base = ENV.fetch("SECRET_KEY_BASE", "reservation-management-sample-secret-key-base")
+  end
+end
---
+++ examples/resavation_management/config/environments/test.rb
@@ -0,0 +1,9 @@
+Rails.application.configure do
+  config.enable_reloading = false
+  config.eager_load = false
+  config.consider_all_requests_local = true
+  config.action_controller.allow_forgery_protection = false
+  config.active_support.deprecation = :stderr
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: "example.test" }
+end
